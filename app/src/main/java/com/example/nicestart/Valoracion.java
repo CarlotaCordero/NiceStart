300d1c1828aae1f60ccbc8b9b698051098c88a68
“Ignacio <ignacio.rc1@gmail.com>
2023-11-21 09:17:38 +0100

    Añadida activty Valoracion para rull request

---
+++ app/src/main/java/com/example/nicestart/Valoracion.java
@@ -0,0 +1,28 @@
+package com.example.nicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class Valoracion extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private Button Alerta;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valoracion);
+
+        ratingBar = (RatingBar) findViewById(R.id.Puntuacion);
+        Alerta = (Button) findViewById(R.id.Alert);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(Valoracion.this,"Has votado con "+ rating,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
